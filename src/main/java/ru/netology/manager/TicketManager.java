8970e2b8a4030c0ac1e451989f58d822cc9d7f0b
Valeriia <lera-bezborodova@mail.ru>
2020-06-29 00:34:17 +0300

    Initial commit

---
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -0,0 +1,40 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Ticket;
+import ru.netology.repository.TicketRepository;
+
+import java.util.Arrays;
+
+public class TicketManager {
+    private TicketRepository repository;
+
+    public TicketManager(TicketRepository repository) {
+        this.repository = repository;
+    }
+
+    public void save(Ticket item) {
+        repository.save(item);
+    }
+
+    public boolean matches(Ticket ticket, String from, String to) {
+        if (ticket.getFrom().equalsIgnoreCase(from) && ticket.getTo().equalsIgnoreCase(to)) {
+            return true;
+        }
+        return false;
+    }
+
+    public Ticket[] findByFromAndTo (String from, String to){
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket: repository.findAll()) {
+            if (matches(ticket, from, to)){
+                Ticket[] tmp = new Ticket[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = ticket;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result);
+        return result;
+    }
+
+}
---
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -0,0 +1,54 @@
+package ru.netology.manager;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.domain.Ticket;
+import ru.netology.repository.TicketRepository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TicketManagerTest {
+    private TicketRepository repository = new TicketRepository();
+    private TicketManager manager = new TicketManager(repository);
+    private Ticket ticket1 = new Ticket(1, 1299, "DME", "KZN", 95);
+    private Ticket ticket2 = new Ticket(2, 1500, "DME", "KZN", 95);
+    private Ticket ticket3 = new Ticket(3, 2199, "VKO", "KZN", 95);
+    private Ticket ticket4 = new Ticket(4, 3399, "ARH", "DME", 100);
+
+    @BeforeEach
+    public void save(){
+        manager.save(ticket1);
+        manager.save(ticket2);
+        manager.save(ticket3);
+        manager.save(ticket4);
+    }
+
+    @Test
+    void shouldFind (){
+        Ticket[] actual = manager.findByFromAndTo("DME", "KZN");
+        Ticket[] expected = new Ticket[] {ticket1, ticket2};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindIfNotExist() {
+        Ticket[] actual = manager.findByFromAndTo("DME", "SVO");
+        Ticket[] expected = new Ticket[] {};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindAll (){
+        Ticket[] actual = repository.findAll();
+        Ticket[] expected = new Ticket[] {ticket1, ticket2, ticket3, ticket4};
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    void shouldRemoveById() {
+        repository.removeById(2);
+        Ticket[] expected = new Ticket[] {ticket1, ticket3, ticket4};
+        assertArrayEquals(expected, repository.findAll());
+    }
+
+}
