97844e8de91b775f48ba29d8df85cb5aa428ba7a
Valeriia <lera-bezborodova@mail.ru>
2020-06-29 00:52:45 +0300

    add comparator

--- src/main/java/ru/netology/manager/TicketManager.java
+++ src/main/java/ru/netology/manager/TicketManager.java
@@ -4,6 +4,7 @@ import ru.netology.domain.Ticket;
 import ru.netology.repository.TicketRepository;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class TicketManager {
     private TicketRepository repository;
@@ -34,6 +35,20 @@ public class TicketManager {
             }
         }
         Arrays.sort(result);
+        return result;
+    }
+
+    public Ticket[] findFromAndToAndFaster(String from, String to, Comparator<Ticket> comparator){
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket: repository.findAll()) {
+            if (matches(ticket, from, to)){
+                Ticket[] tmp = new Ticket[result.length + 1];
+                System.arraycopy(result, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = ticket;
+                result = tmp;
+            }
+        }
+        Arrays.sort(result, comparator);
         return result;
     }
 
